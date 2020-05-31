dd82855b06fe5cf30ab45005e61e0a43a13c6cc8
youurt <ugurtigu@gmail.com>
2020-05-31 15:24:11 +0200

    nodemon

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const fs = require('fs');
+
+const app = express();
+const router = express.Router();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/movie_rating_app', () => {
+    console.log('Connection has been made');
+}).catch(err => {
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+});
+
+router.get('/', (req, res) => {
+    res.json({ message: 'API Initialized!' })
+});
+
+const port = process.env.API_PORT || 8081;
+app.use('/', router);
+app.listen(port, () => {
+    console.log(`api running on port ${port}`);
+});
